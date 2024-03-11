62b58563c2ced39f8cbe273bfa8102e1dc65cd2e
ChristinaLLLL <161545793+ChristinaLLLL@users.noreply.github.com>
2024-03-12 11:59:11 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(600, 600);
+
+}
+
+function draw() {
+  background(140,0,33);
+  square (100, 200, 300, 500);
+  square (200,300,44,100);
+  square (400,300,100,100);
+  circle (100,200,33);
+
+
+setTimeout (apple,4000);
+  
+function apple (){
+  fill (200,100,33);
+  circle (100,300,44);
+  circle (200, 33, 500);
+
+
+
+  
+}
+  
+  
+  
+  
+}
